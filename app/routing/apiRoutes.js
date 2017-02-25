17189daad9aa208995d25f1e8430f00967455e79
John Krug <John Krug>
2017-02-25 16:01:55 -0500

    added radio buttons

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,5 +6,9 @@ module.exports = function(app) {
 	// API GET requests
 	app.get("/api/friends", function(req, res) {
 		res.json(friends);
-	})
+	});
+
+	app.post("/api/friends", function(req, res) {
+		friends.push(req.body);
+	});
 }
